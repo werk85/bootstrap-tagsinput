5dc310df0615fb9eee07e01d4ef2226377c02cbb
David Zacharias <david@werk85.de>
2019-11-26 16:39:37 +0100

    Fix for size issue. See https://github.com/bootstrap-tagsinput/bootstrap-tagsinput/pull/462

--- src/bootstrap-tagsinput.js
+++ src/bootstrap-tagsinput.js
@@ -455,7 +455,7 @@
         var textLength = $input.val().length,
             wordSpace = Math.ceil(textLength / 5),
             size = textLength + wordSpace + 1;
-        $input.attr('size', Math.max(this.inputSize, $input.val().length));
+        $input.attr('size', Math.max(this.inputSize, size));
       }, self));
 
       self.$container.on('keypress', 'input', $.proxy(function(event) {
@@ -485,7 +485,7 @@
          var textLength = $input.val().length,
             wordSpace = Math.ceil(textLength / 5),
             size = textLength + wordSpace + 1;
-         $input.attr('size', Math.max(this.inputSize, $input.val().length));
+         $input.attr('size', Math.max(this.inputSize, size));
       }, self));
 
       // Remove icon clicked
